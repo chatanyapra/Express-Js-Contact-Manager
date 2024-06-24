66cf459c95ffafcd15595f69ebf9c06f562305da
chatanyapra <chatanyapra@gmail.com>
2024-06-24 13:18:19 +0530

    jwt token applied to validate user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../model/userModel');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async (req, res) => {
     const {username, email, password} = req.body;
@@ -8,6 +9,7 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("All field are mandatory!");
     }
+    console.log(username, email, password);
     const userAvaliable= await User.findOne({email});
     if(userAvaliable){
         res.status(400);
@@ -28,4 +30,45 @@ const registerUser = asyncHandler(async (req, res) => {
     res.json({message: "Register the user"});
 })
 
-module.exports= registerUser;
+const loginUser= asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user= await User.findOne({email});
+    // compare the password with hashed password
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken= jwt.sign(
+            {
+                user:{
+                    username: user.username,
+                    email: user.email,
+                    id: user.id
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: "5m"}
+        )
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("Email or Password not valid!");
+    }
+})
+
+// @desc GET current information
+// @route GET api/user/current
+// access private
+const currentUser = asyncHandler(async(req, res) => {
+    res.json(req.user);
+})
+
+
+const getUserDetails = asyncHandler(async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
+});
+
+
+module.exports= {registerUser, loginUser, currentUser, getUserDetails};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const registerUser = require('../controllers/userController');
+const {registerUser, loginUser, currentUser, getUserDetails} = require('../controllers/userController');
+const validateToken = require("../middleware/validateTokenHandler");
 
 // router.route("/").get(getContact);
 
-router.route("/").post(registerUser);
+router.post("/register", registerUser);
 
+router.get("/users", getUserDetails);
+
+router.post("/login", loginUser);
+
+router.get("/current", validateToken, currentUser);
 // router.route("/:id").get(getIDContact);
 
 // router.route("/:id").put(updateContact);
